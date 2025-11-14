be8bbb0b8bb4ec2e23bf70a13707784dc1f703eb
NachoBarcelo <nachobarcelo25@gmail.com>
2025-11-14 19:06:38 -0300

    Pantalla de carga agregada

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import { Navbar, type NavItem } from "./components/Navbar";
 import { HomeLanding, LoginForm, TournamentDetails, RegisterForm, About, Contact, Faq } from "./public";
-import { Logo } from "./components/Logo";
+import { useEffect, useState } from "react";
 import { Footer } from "./components/Footer";
 import Dashboard from "./private/dashboard/Dashboard";
 import { useGlobalContext } from "./context/global.context";
 import { PrivateRoute } from "./components/PrivateRoute/PrivateRoute";
-import { RegisterFormNew } from "./public/RegisterFormNew";
+import { SplashScreen } from "./public/SplashScreen";
 
 
 function App() {
   const { token, logout } = useGlobalContext()
   const navigate = useNavigate();
   const location = useLocation();
+
+
 
   const hideChrome = location.pathname.startsWith("/login") || location.pathname.startsWith("/signup");
 
@@ -26,6 +28,19 @@ function App() {
     logout();
     navigate("/");
   };
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1200); // 1.2s
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <SplashScreen />; // pantalla de carga
+  }
   return (
     <>
 
@@ -66,13 +81,13 @@ function App() {
             path="/signup"
             element={
               <PrivateRoute isPrivate={false}>
-                <RegisterFormNew />
+                <RegisterForm />
               </PrivateRoute>
             } />
         </Routes>
       </main>
 
-      
+
 
       {!hideChrome && <Footer />}
 
